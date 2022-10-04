7b6ab4d13a6e220628294315d23abc0b0ef08eb3
ALANFER13 <alanfersp13@gmail.com>
2022-10-04 15:53:48 -0500

    primer commit

---
+++ src/app/calculos/operas-bas/operas-bas.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  templateUrl: './operas-bas.component.html',
+  styleUrls: ['./operas-bas.component.css']
+})
+export class OperasBasComponent implements OnInit {
+  num1:string=''
+  num2:string=''
+  opc:string='';
+
+  resultado:number=0;
+
+  constructor() { }
+  operacion():void{
+    if (this.opc=="suma" || this.opc=="Suma") {
+      this.suma();
+    }
+    else if (this.opc=="resta" || this.opc=="Resta") {
+      this.resta();
+    }
+    else if (this.opc=="multiplicar" || this.opc=="Multiplicar") {
+      this.mult();
+    }
+    else if (this.opc=="dividir" || this.opc=="Dividir") {
+      this.div();
+    }
+  }
+
+  suma():void{
+    this.resultado=parseInt(this.num1)+parseInt(this.num2);
+  }
+  resta():void{
+    this.resultado=parseInt(this.num1)-parseInt(this.num2);
+  }
+  mult():void{
+    this.resultado=parseInt(this.num1)*parseInt(this.num2);
+  }
+  div():void{
+    this.resultado=parseInt(this.num1)/parseInt(this.num2);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/productos/product-list/product-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { IProducto } from '../producto';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  imageWidth:number=50;
+  imageMargin:number=2;
+
+  muestraImg:boolean=true;
+  listFilter:string;
+  productos:IProducto[]=[
+    {
+      "productoID":1,
+      "modelo":'sentra',
+      "descripcion":"auto 4 puertas",
+      "marca":'Nissan',
+      "year":"febrero 3 2020",
+      "precio":250000,
+      "puntos":3.3,
+      "ImagenUrl":"https://img.automexico.com/crop/840x640/2020/07/07/rkwP8Fam/nissan-sentra-2020-1280-11-059a.jpg"
+
+    },
+    {
+      "productoID":1,
+      "modelo":'Rio',
+      "descripcion":"auto 4 puertas",
+      "marca":'Kia',
+      "year":"december 20 2020",
+      "precio":150000,
+      "puntos":4.3,
+      "ImagenUrl":"https://www.kia.com/content/dam/kwcms/mx/es/images/showroom/2022/rio-sd-bn/360/exterior/spack/sporty-blue/SportyBlue_0000.png"
+
+    },
+    {
+      "productoID":1,
+      "modelo":'March',
+      "descripcion":"auto 2 puertas",
+      "marca":'Nissan',
+      "year":"marzo 24 2020",
+      "precio":150000,
+      "puntos":4.3,
+      "ImagenUrl":"https://www.elcarrocolombiano.com/wp-content/uploads/2020/08/20200821-NISSAN-MARCH-ACTIVE-REDUCCION-DE-PRECIO-EN-COLOMBIA-01.jpg"
+
+    }
+
+  ]
+
+  constructor() { }
+
+  ShowImagen():void{
+    this.muestraImg= !this.muestraImg;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
